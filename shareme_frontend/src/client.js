c6e3822abf79919037cb95c015d42f38c0afa3e6
hetvi-11 <hetvidesai4010@gmail.com>
2024-03-26 00:46:42 -0700

    Google OAuth sign-in completed

--- shareme_frontend/src/client.js
+++ shareme_frontend/src/client.js
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: "21y5ejrc",
   dataset: "production",
   apiVersion: "2021-11-16",
-  useCdn: true,
+  useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,44 +1,3 @@
-// import React from "react";
-// import shareVideo from "../assets/share.mp4";
-// import logo from "../assets/logowhite.png";
-// import { GoogleLogin } from "@react-oauth/google";
-// // import { useNavigate } from 'react-router-dom';
-// import { FcGoogle } from "react-icons/fc";
-
-// const Login = () => {
-//   return (
-//     <div className="flex justify-start items-center flex-col h-screen">
-//       <div className="relative w-full h-full">
-//         <video
-//           src={shareVideo}
-//           type="video/mp4"
-//           Loop
-//           controls={false}
-//           muted
-//           autoPlay
-//           className="w-full h-full object-cover"
-//         />
-//         <div className="absolute flex flex-col justify-center items-center top-0 right-0 bottom-0 left-0 bg-blackOverlay">
-//           <div className="p-5">
-//             <img src={logo} width="130px" alt="logo" />
-//           </div>
-//           <div className="shadow-2xl">
-//             <GoogleLogin
-//               clientID=""
-//               render={(renderProps) => (
-//                 <button type="button" className="bg-mainColor">
-//                   <FcGoogle className="mr-4" /> Sign in with Google
-//                 </button>
-//               )}
-//             ></GoogleLogin>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
@@ -46,27 +5,43 @@ import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 import { client } from "../client";
 
 const Login = () => {
   const navigate = useNavigate();
+
   const responseGoogle = (response) => {
-    localStorage.setItem("user", JSON.stringify(response.profileObj));
-    const { name, googleId, imageUrl } = response.profileObj;
-    const doc = {
-      _id: googleId,
-      _type: "user",
-      userName: name,
-      image: imageUrl,
-    };
-    client.createIfNotExists(doc).then(() => {
-      navigate("/", { replace: true });
-    });
+    if (response.credential) {
+      const decoded = jwtDecode(response.credential);
+      console.log(decoded); // You can see the structure of the decoded data
+
+      localStorage.setItem("user", JSON.stringify(decoded));
+      const { name, sub: googleId, picture: imageUrl } = decoded;
+      const doc = {
+        _id: googleId,
+        _type: "user",
+        userName: name,
+        image: imageUrl,
+      };
+      console.log(doc);
+      console.log(client.config());
+      client
+        .createIfNotExists(doc)
+        .then(() => navigate("/", { replace: true }))
+        .catch((error) =>
+          console.error("Error creating document in Sanity:", error)
+        );
+    } else {
+      // Handle error or unsuccessful login
+    }
   };
 
   return (
-    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+    <GoogleOAuthProvider
+      clientId={`73926476617-t0hbv6d1l0i5rrh134hlhmeje49gjd3l.apps.googleusercontent.com`}
+    >
       <div className="flex justify-start items-center flex-col h-screen">
         <div className=" relative w-full h-full">
           <video
